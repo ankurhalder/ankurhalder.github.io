5a59f1b601ec6497ac8b8ffc7a3d9c4ad72add41
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-04-24 17:01:53 +0530

    Refactor styles and remove unused components

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,30 @@
 function App() {
   return (
     <div>
-      <section>Hero</section>
-      <section>parallex</section>
-      <section>Services</section>
-      <section>Parallex</section>
-      <section>Portfolio1</section>
-      <section>Portfolio2</section>
-      <section>Portfolio3</section>
-      <section>Contact</section>
-
-      {/* <Layout /> */}
+      <section>
+        <a href="hero">Hero</a>
+      </section>
+      <section>
+        <a href="parallex">Parallex</a>
+      </section>
+      <section>
+        <a href="services">Services</a>
+      </section>
+      <section>
+        <a href="parallex">Parallex</a>
+      </section>
+      <section>
+        <a href="portfolio1">Portfolio1</a>
+      </section>
+      <section>
+        <a href="portfolio2">Portfolio2</a>
+      </section>
+      <section>
+        <a href="portfolio3">Portfolio3</a>
+      </section>
+      <section>
+        <a href="contact">Contact</a>
+      </section>
     </div>
   );
 }
