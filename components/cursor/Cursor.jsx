52cbce823080e4ddcace35f130ea241ea3546d74
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-04-26 16:56:41 +0530

    Update Cursor animation in Cursor.jsx

--- components/cursor/Cursor.jsx
+++ components/cursor/Cursor.jsx
@@ -19,7 +19,7 @@ const Cursor = () => {
   return (
     <motion.div
       className="cursor"
-      animate={{ x: position.x + 10, y: position.y + 10 }}
+      animate={{ x: position.x + 1, y: position.y + 1 }}
     ></motion.div>
   );
 };
