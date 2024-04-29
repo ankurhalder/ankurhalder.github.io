7089975ac94a7b7d4656b7d83c0698de31a5320f
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-04-29 19:13:18 +0530

    Update Parallax component text and styles

--- components/parallax/Parallax.jsx
+++ components/parallax/Parallax.jsx
@@ -26,7 +26,9 @@ const Parallax = ({ type }) => {
       }}
     >
       <motion.h1 style={{ y: yText }}>
-        {type === "services" ? "What We Do?" : "What We Did?"}
+        {type === "services"
+          ? "Looking for a Skill Set That Matches Your Needs?"
+          : "What We Did?"}
       </motion.h1>
       <motion.div className="mountains"></motion.div>
       <motion.div
