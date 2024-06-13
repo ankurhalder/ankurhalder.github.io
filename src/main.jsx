4c877f19c4a50f59d3956c20bf398674f9bf0235
Ankur Halder <ankur.halder12345@gmail.com>
2024-06-13 22:58:01 +0530

    refactor: Update package dependencies and optimize image loading
    Update package dependencies in package-lock.json and package.json files. Add "@vercel/analytics" version "^1.3.1" and "@vercel/speed-insights" version "^1.0.12". Optimize image loading in various components.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "../styles/main.scss";
-
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Analytics />
+    <SpeedInsights />
     <App />
   </React.StrictMode>
 );
