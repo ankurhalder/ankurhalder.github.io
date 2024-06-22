09a84df541218996c69a303f924e2ce5f94bbe4b
Ankur Halder <ankur.halder12345@gmail.com>
2024-06-22 23:05:17 +0530

    refactor: Update Parallax component with lazy loading for images

--- components/parallax/Parallax.jsx
+++ components/parallax/Parallax.jsx
@@ -50,7 +50,7 @@ const Parallax = ({ type, isDarkMode }) => {
     [0, 1],
     viewportWidth >= 450 ? ["0deg", "45deg"] : ["0deg", "0deg"]
   );
-  const rotateSky = useTransform(scrollYProgress, [0, 1], ["0deg", "0deg"]);
+  const rotateSky = useTransform(scrollYProgress, [0, 1], ["0deg", "1deg"]);
   const moveCloud1 = useTransform(scrollYProgress, [0, 1], ["-20%", "20%"]);
   const moveCloud2 = useTransform(scrollYProgress, [0, 1], ["-30%", "30%"]);
 
