10c6ce6efd9b5756be3d10f35bd36c426525b3a9
Ankur Halder <ankur.halder12345@gmail.com>
2024-06-25 17:16:43 +0530

    refactor: Update MainProjects component to remove unused code and add "Featured Projects" heading

--- components/mainprojects/MainProjects.jsx
+++ components/mainprojects/MainProjects.jsx
@@ -195,7 +195,6 @@ const MainProject = () => {
           <motion.h1
             ref={inViewRef}
             className="projectsTitle"
-            // ref={ref}
             initial={{ opacity: 0, scale: 0.8 }}
             animate={
               inView ? { opacity: 1, scale: 1 } : { opacity: 0, scale: 0.8 }
