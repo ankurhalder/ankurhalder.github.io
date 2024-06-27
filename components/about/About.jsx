ab9613c41926b65fdbc8efe62c5eaffcabbea51a
Ankur Halder <ankur.halder12345@gmail.com>
2024-06-27 15:59:10 +0530

    basic about structure created

---
+++ components/about/About.jsx
@@ -0,0 +1,29 @@
+import { Fragment } from "react";
+
+function About() {
+  return (
+    <Fragment>
+      <article>
+        <h2>Hello, I`&apos;`m Ankur Halder 👋</h2>
+        <p>
+          I am a dedicated software developer proficient in the MERN stack,
+          Django (full stack, REST API, ORM), Python (OOP), JavaScript (OOP),
+          Java (OOP), and Next.js (full stack), with a strong focus on creating
+          robust and efficient solutions. Currently pursuing a B.Tech in
+          Computer Science at the University Of Engineering & Management, New
+          Town, I bring a solid foundation in data structures and algorithms to
+          my work.
+        </p>
+        <p>
+          My educational journey includes Senior Secondary studies in Science
+          and Secondary education, providing me with a comprehensive academic
+          background. Beyond coding, I find joy in exploring diverse interests
+          like watching anime, discovering new movies and TV series, and diving
+          into captivating science fiction and space exploration topics.
+        </p>
+      </article>
+    </Fragment>
+  );
+}
+
+export default About;
